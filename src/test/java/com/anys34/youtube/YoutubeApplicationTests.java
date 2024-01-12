62c373f1ecd1aa1146257070218e53ecc58bc304
anys34 <anyeseong34@gmail.com>
2024-01-13 01:29:50 +0900

    DELETE :: test code

--- src/test/java/com/anys34/youtube/YoutubeApplicationTests.java
+++ src/test/java/com/anys34/youtube/YoutubeApplicationTests.java
@@ -1,13 +1,8 @@
 package com.anys34.youtube;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YoutubeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
