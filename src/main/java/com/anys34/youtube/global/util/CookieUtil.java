8fc3bdf49eb371d65a291f748e15068b1ad0e2f7
anys34 <anyeseong34@gmail.com>
2024-01-13 20:24:42 +0900

    ADD :: setDomain

--- src/main/java/com/anys34/youtube/global/util/CookieUtil.java
+++ src/main/java/com/anys34/youtube/global/util/CookieUtil.java
@@ -11,7 +11,7 @@ public class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
-//        cookie.setDomain("localhost");
+        cookie.setDomain("localhost");
         cookie.setHttpOnly(true);
         cookie.setMaxAge(maxAge);
         cookie.setSecure(true);
