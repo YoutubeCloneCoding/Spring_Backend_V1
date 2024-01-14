8c5c992a03f2ecef78354111c8ff131172da8200
anys34 <anyeseong34@gmail.com>
2024-01-14 23:06:32 +0900

    FIX :: allowCredentials

--- src/main/java/com/anys34/youtube/global/config/corsConfig.java
+++ src/main/java/com/anys34/youtube/global/config/corsConfig.java
@@ -10,6 +10,7 @@ public class corsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
