de57c5861f6a8e3eee870bae3cc6c5b007920fcd
anys34 <anyeseong34@gmail.com>
2024-01-17 16:02:31 +0900

    ADD :: originDir

--- src/main/java/com/anys34/youtube/domain/File/presentation/FileController.java
+++ src/main/java/com/anys34/youtube/domain/File/presentation/FileController.java
@@ -3,6 +3,7 @@ package com.anys34.youtube.domain.File.presentation;
 import com.anys34.youtube.domain.File.domain.type.FileType;
 import com.anys34.youtube.domain.File.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class FileController {
+    @Value("${spring.save.dir}")
+    private String originDir;
+
     private final FileService fileService;
 
     @GetMapping("/{fileName}")
     public ResponseEntity<Resource> showFile(@RequestParam(value = "email") String email,
                                              @RequestParam(value = "type") FileType fileType,
                                              @PathVariable String fileName) {
-        String dirPath = "src/main/resources/save/" + email + "/" + fileType;
+        String dirPath = originDir + email + "/" + fileType;
         Resource resource = fileService.showFile(dirPath, fileName);
 
         HttpHeaders headers = new HttpHeaders();
