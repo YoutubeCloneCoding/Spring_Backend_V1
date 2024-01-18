87cc342f3c65aeb4ebead31ee20eeb44e4966c5c
anys34 <anyeseong34@gmail.com>
2024-01-18 20:34:46 +0900

    ADD :: link

--- src/main/java/com/anys34/youtube/domain/Video/domain/repository/VideoRepository.java
+++ src/main/java/com/anys34/youtube/domain/Video/domain/repository/VideoRepository.java
@@ -1,7 +1,9 @@
 package com.anys34.youtube.domain.Video.domain.repository;
 
+import com.anys34.youtube.domain.Post.domain.Post;
 import com.anys34.youtube.domain.Video.domain.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    Video findByPost(Post post);
 }
