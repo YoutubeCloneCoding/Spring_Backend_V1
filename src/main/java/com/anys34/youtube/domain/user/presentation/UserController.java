c1747ee1211095b36159c40b5b7eb9b74a981e7d
anys34 <anyeseong34@gmail.com>
2024-01-19 18:10:12 +0900

    ADD :: get profile

--- src/main/java/com/anys34/youtube/domain/user/presentation/UserController.java
+++ src/main/java/com/anys34/youtube/domain/user/presentation/UserController.java
@@ -6,6 +6,7 @@ import com.anys34.youtube.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -18,6 +19,17 @@ public class UserController {
     @GetMapping("/api/profile")
     public ResponseEntity<UserInfoResponse> profile(Principal principal) {
         User user = userService.findByEmail(principal.getName());
+        return ResponseEntity.ok()
+                .body(UserInfoResponse.builder()
+                        .nickname(user.getNickname())
+                        .email(user.getEmail())
+                        .profile(user.getProfileImg())
+                        .build());
+    }
+
+    @GetMapping("/profile/{email}")
+    public ResponseEntity<UserInfoResponse> profile(@PathVariable String email) {
+        User user = userService.findByEmail(email);
         return ResponseEntity.ok()
                 .body(UserInfoResponse.builder()
                         .nickname(user.getNickname())
