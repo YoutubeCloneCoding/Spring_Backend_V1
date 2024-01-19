47682a2d0002db2601cdac1887ee276a4463c50a
anys34 <anyeseong34@gmail.com>
2024-01-19 15:12:27 +0900

    ADD :: Redis

--- src/main/java/com/anys34/youtube/domain/refreshToken/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/anys34/youtube/domain/refreshToken/domain/repository/RefreshTokenRepository.java
@@ -1,11 +1,11 @@
 package com.anys34.youtube.domain.refreshToken.domain.repository;
 
 import com.anys34.youtube.domain.refreshToken.domain.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(Long userId);
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
