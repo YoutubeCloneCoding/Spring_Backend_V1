b4f70fae78bafceff45ae59013dcdf0893f99754
anys34 <anyeseong34@gmail.com>
2024-01-20 00:00:41 +0900

    ADD :: facade

---
+++ src/main/java/com/anys34/youtube/domain/user/facade/UserFacade.java
@@ -0,0 +1,24 @@
+package com.anys34.youtube.domain.user.facade;
+
+import com.anys34.youtube.domain.user.domain.User;
+import com.anys34.youtube.domain.user.domain.repository.UserRepository;
+import com.anys34.youtube.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@RequiredArgsConstructor
+@Configuration
+public class UserFacade {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
