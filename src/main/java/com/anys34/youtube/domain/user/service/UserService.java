23db3488a13402ab40e8df2e8c8431b2310af691
anys34 <anyeseong34@gmail.com>
2024-01-19 23:47:21 +0900

    ADD :: UserNotFoundException

--- src/main/java/com/anys34/youtube/domain/user/service/UserService.java
+++ src/main/java/com/anys34/youtube/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.anys34.youtube.domain.user.service;
 
 import com.anys34.youtube.domain.user.domain.User;
 import com.anys34.youtube.domain.user.domain.repository.UserRepository;
+import com.anys34.youtube.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,17 @@ public class UserService {
 
     public User findById(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 
     public User getUser() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 }
