750d2642855b186cf5dc286e7e2382adee0065ff
anys34 <anyeseong34@gmail.com>
2024-01-20 00:38:00 +0900

    ADD :: Transactional

--- src/main/java/com/anys34/youtube/domain/user/service/UserIdFindService.java
+++ src/main/java/com/anys34/youtube/domain/user/service/UserIdFindService.java
@@ -5,12 +5,14 @@ import com.anys34.youtube.domain.user.domain.repository.UserRepository;
 import com.anys34.youtube.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserIdFindService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public User execute(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
--- src/main/java/com/anys34/youtube/domain/user/service/UserInfoService.java
+++ src/main/java/com/anys34/youtube/domain/user/service/UserInfoService.java
@@ -5,12 +5,14 @@ import com.anys34.youtube.domain.user.domain.repository.UserRepository;
 import com.anys34.youtube.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserInfoService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public User execute(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
