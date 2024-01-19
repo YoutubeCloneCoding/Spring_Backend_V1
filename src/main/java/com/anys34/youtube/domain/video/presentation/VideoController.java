9c2a01baea61585af0982db7f2b1f5399644baf7
anys34 <anyeseong34@gmail.com>
2024-01-20 00:25:15 +0900

    ADD :: currentUser

--- src/main/java/com/anys34/youtube/domain/video/presentation/VideoController.java
+++ src/main/java/com/anys34/youtube/domain/video/presentation/VideoController.java
@@ -1,7 +1,8 @@
 package com.anys34.youtube.domain.video.presentation;
 
 import com.anys34.youtube.domain.user.domain.User;
-import com.anys34.youtube.domain.user.service.UserService;
+import com.anys34.youtube.domain.user.facade.UserFacade;
+import com.anys34.youtube.domain.user.service.LoginUserInfoService;
 import com.anys34.youtube.domain.video.presentation.dto.res.ReturnInfoResponse;
 import com.anys34.youtube.domain.video.presentation.dto.res.VideoReturnResponse;
 import com.anys34.youtube.domain.video.service.VideoService;
@@ -17,11 +18,11 @@ import java.util.UUID;
 @RestController
 public class VideoController {
     private final VideoService videoService;
-    private final UserService userService;
+    private final UserFacade userFacade;
 
     @PostMapping("/api/upload")
-    public ResponseEntity<ReturnInfoResponse> upload(@RequestParam("file") MultipartFile file, Principal principal) {
-        User user = userService.findByEmail(principal.getName());
+    public ResponseEntity<ReturnInfoResponse> upload(@RequestParam("file") MultipartFile file) {
+        User user = userFacade.getCurrentUser();
         ReturnInfoResponse response = videoService.upload(file, user);
         return ResponseEntity.ok()
                 .body(response);
