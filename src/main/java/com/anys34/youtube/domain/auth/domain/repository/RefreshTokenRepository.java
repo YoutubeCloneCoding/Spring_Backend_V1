741b9a82c1150a8129309281da4bee93a0c9c0d8
anys34 <anyeseong34@gmail.com>
2024-02-08 19:42:58 +0900

    Refactor :: findByUserId로 변경

--- src/main/java/com/anys34/youtube/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/anys34/youtube/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByEmail(Long userId);
+    Optional<RefreshToken> findByUserId(Long userId);
 }
