9e844a0b386f853047ff992c718e8cfaf6a2af44
anys34 <anyeseong34@gmail.com>
2024-02-08 19:37:59 +0900

    Refactor :: Service 제거

--- src/main/java/com/anys34/youtube/domain/auth/service/CreateNewAccessToken.java
+++ src/main/java/com/anys34/youtube/domain/auth/service/CreateNewAccessToken.java
@@ -3,7 +3,8 @@ package com.anys34.youtube.domain.auth.service;
 import com.anys34.youtube.domain.auth.domain.repository.RefreshTokenRepository;
 import com.anys34.youtube.domain.auth.exception.UnexpectedTokenException;
 import com.anys34.youtube.domain.user.domain.User;
-import com.anys34.youtube.domain.user.service.UserIdFindService;
+import com.anys34.youtube.domain.user.domain.repository.UserRepository;
+import com.anys34.youtube.domain.user.exception.UserNotFoundException;
 import com.anys34.youtube.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ import java.time.Duration;
 @Service
 public class CreateNewAccessToken {
     private final RefreshTokenRepository refreshTokenRepository;
-    private final UserIdFindService userIdFindService;
+    private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
 
     @Transactional(readOnly = true)
@@ -25,7 +26,8 @@ public class CreateNewAccessToken {
 
         Long userId = refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> UnexpectedTokenException.EXCEPTION).getEmail();
-        User user = userIdFindService.execute(userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
         return tokenProvider.generateToken(user, Duration.ofHours(2));
     }
