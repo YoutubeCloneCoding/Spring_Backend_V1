09b42da94ce4ec9f5f560c73dbd41e254ea5cfe2
anys34 <anyeseong34@gmail.com>
2024-02-09 01:26:53 +0900

    Fix :: Transactional 추가

--- src/main/java/com/anys34/youtube/domain/user/service/LoginUserInfoService.java
+++ src/main/java/com/anys34/youtube/domain/user/service/LoginUserInfoService.java
@@ -4,12 +4,14 @@ import com.anys34.youtube.domain.user.domain.User;
 import com.anys34.youtube.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class LoginUserInfoService {
     private final UserFacade userFacade;
 
+    @Transactional(readOnly = true)
     public User execute() {
         return userFacade.getCurrentUser();
     }
