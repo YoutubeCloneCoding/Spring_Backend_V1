f98ffb2161648c3c37f49a633de17e805a6f5282
anys34 <anyeseong34@gmail.com>
2024-02-08 19:25:19 +0900

    Refactor :: 기본 생성자와 Getter생성

--- src/main/java/com/anys34/youtube/domain/video/presentation/dto/req/VideoUploadRequest.java
+++ src/main/java/com/anys34/youtube/domain/video/presentation/dto/req/VideoUploadRequest.java
@@ -3,9 +3,11 @@ package com.anys34.youtube.domain.video.presentation.dto.req;
 import com.anys34.youtube.domain.post.domain.Post;
 import com.anys34.youtube.domain.post.domain.type.PublicScope;
 import com.anys34.youtube.domain.user.domain.User;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+@Getter
 @NoArgsConstructor
 public class VideoUploadRequest {
     private MultipartFile video;
