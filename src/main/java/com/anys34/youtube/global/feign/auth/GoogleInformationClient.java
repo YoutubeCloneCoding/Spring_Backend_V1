b594223faa186ce886ee7716ccb09a4a4d6d9450
anys34 <anyeseong34@gmail.com>
2024-02-09 03:25:17 +0900

    Fix :: public 제거

--- src/main/java/com/anys34/youtube/global/feign/auth/GoogleInformationClient.java
+++ src/main/java/com/anys34/youtube/global/feign/auth/GoogleInformationClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "GoogleInformationClient", url = "https://www.googleapis.com/oauth2/v1/userinfo")
 public interface GoogleInformationClient {
     @GetMapping("?alt=json&access_token={TOKEN}")
-    public GoogleInformationResponse getUserInformation(@PathVariable("TOKEN") String accessToken);
+    GoogleInformationResponse getUserInformation(@PathVariable("TOKEN") String accessToken);
 }
