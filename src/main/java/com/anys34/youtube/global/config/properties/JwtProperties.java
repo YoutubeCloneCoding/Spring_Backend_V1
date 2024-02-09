26a572162cf12819af35b7287ade890af38c3a10
anys34 <anyeseong34@gmail.com>
2024-02-10 02:34:17 +0900

    Refactor :: jwt Properties

--- src/main/java/com/anys34/youtube/global/config/properties/JwtProperties.java
+++ src/main/java/com/anys34/youtube/global/config/properties/JwtProperties.java
@@ -3,13 +3,16 @@ package com.anys34.youtube.global.config.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Setter
 @Getter
-@Component
+@Configuration
 @ConfigurationProperties("jwt")
 public class JwtProperties {
-    private String issuer;
+    private Long accessTime;
+    private Long refreshTime;
+    private String prefix;
+    private String header;
     private String secretKey;
 }
