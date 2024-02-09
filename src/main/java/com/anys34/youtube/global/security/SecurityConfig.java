2030aba75b7736a4121abe34f36e3248e28929c1
anys34 <anyeseong34@gmail.com>
2024-02-10 02:41:29 +0900

    Refactor :: SecurityConfig

--- src/main/java/com/anys34/youtube/global/security/SecurityConfig.java
+++ src/main/java/com/anys34/youtube/global/security/SecurityConfig.java
@@ -1,29 +1,24 @@
 package com.anys34.youtube.global.security;
 
-import com.anys34.youtube.domain.auth.domain.repository.RefreshTokenRepository;
-import com.anys34.youtube.domain.user.facade.UserFacade;
-import com.anys34.youtube.global.security.jwt.TokenProvider;
-import com.anys34.youtube.global.security.auth.CustomAuthenticationEntryPoint;
-import com.anys34.youtube.global.security.auth.OAuth2SuccessHandler;
-import com.anys34.youtube.global.security.auth.OAuth2UserCustomService;
+import com.anys34.youtube.global.security.jwt.JwtTokenFilter;
+import com.anys34.youtube.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-    private final OAuth2UserCustomService oAuth2UserCustomService;
-    private final TokenProvider tokenProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final UserFacade userFacade;
+    private final JwtTokenProvider jwtProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,39 +30,19 @@ public class SecurityConfig {
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtTokenFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter.class);
 
-        http.cors().and().
-                authorizeRequests()
+        http.cors().and()
+                .authorizeRequests()
                 .requestMatchers("/video").authenticated()
                 .requestMatchers("/user").authenticated()
                 .requestMatchers("/post").authenticated()
                 .anyRequest().permitAll();
 
-        http.oauth2Login()
-                .authorizationEndpoint()
-                .and()
-                .successHandler(oAuth2SuccessHandler())
-                .userInfoEndpoint()
-                .userService(oAuth2UserCustomService);
+        http.exceptionHandling().authenticationEntryPoint(
+                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
-        http.exceptionHandling()
-                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
-                        new AntPathRequestMatcher("/api/**"))
-                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
         return http.build();
-    }
-
-    @Bean
-    public OAuth2SuccessHandler oAuth2SuccessHandler() {
-        return new OAuth2SuccessHandler(tokenProvider,
-                refreshTokenRepository,
-                userFacade
-        );
-    }
-
-    @Bean
-    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-        return new TokenAuthenticationFilter(tokenProvider);
     }
 }
