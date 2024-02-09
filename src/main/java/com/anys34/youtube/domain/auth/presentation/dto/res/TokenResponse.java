fea9c067d1b354e5ffc7c1d98c8a58d8dc07b943
anys34 <anyeseong34@gmail.com>
2024-02-10 02:40:10 +0900

    Refactor :: GoogleAuthService

--- src/main/java/com/anys34/youtube/domain/auth/presentation/dto/res/TokenResponse.java
+++ src/main/java/com/anys34/youtube/domain/auth/presentation/dto/res/TokenResponse.java
@@ -1,10 +1,12 @@
 package com.anys34.youtube.domain.auth.presentation.dto.res;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
--- src/main/java/com/anys34/youtube/domain/auth/service/GoogleAuthService.java
+++ src/main/java/com/anys34/youtube/domain/auth/service/GoogleAuthService.java
@@ -6,6 +6,7 @@ import com.anys34.youtube.domain.user.domain.User;
 import com.anys34.youtube.domain.user.domain.repository.UserRepository;
 import com.anys34.youtube.global.feign.auth.GoogleInformationClient;
 import com.anys34.youtube.global.feign.auth.dto.res.GoogleInformationResponse;
+import com.anys34.youtube.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,25 +17,27 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class GoogleAuthService {
     private final GoogleInformationClient googleInformationClient;
+    private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
 
     @Transactional
-    public TokenResponse execute(AccessTokenRequest accessTokenRequest) {
-        GoogleInformationResponse response = googleInformationClient.getUserInformation(accessTokenRequest.getAccessToken());
-
-        Optional<User> user = userRepository.findByEmail(response.getEmail());
+    public TokenResponse execute(String accessToken) {
+        GoogleInformationResponse response = googleInformationClient
+                .getUserInformation(accessToken);
+        String email = response.getEmail();
+        Optional<User> user = userRepository.findByEmail(email);
 
         if (user.isEmpty()) {
             userRepository.save(User.builder()
-                    .email(response.getEmail())
+                    .email(email)
                     .nickname(response.getName())
                     .profileImg(response.getPicture())
                     .build());
         }
 
         return TokenResponse.builder()
-                .accessToken(null)
-                .refreshToken(null)
+                .accessToken(jwtTokenProvider.createAccessToken(email))
+                .refreshToken(jwtTokenProvider.createRefreshToken(email))
                 .build();
     }
 }
